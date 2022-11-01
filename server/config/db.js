6f1a1d5fce83ba4d8991e5fa26f292d0d1d2d877
Rahabb <rhossa16@my.centennialcollege.ca>
2022-10-31 22:49:23 -0400

    Assignment2

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+module.exports =
+
+{
+    //"URI": "mongodb://localhost/Business"
+
+    "URI": "mongodb+srv://RahabH:YrfoNMoQU1XOgUUs@mongodbserver.lcx34gh.mongodb.net/?retryWrites=true&w=majority"
+}
